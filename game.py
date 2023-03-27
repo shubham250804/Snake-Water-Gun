6f803058769dabf362735d587eb81d072a21ba0d
shubham250804 <97142798+shubham250804@users.noreply.github.com>
2023-03-27 13:56:21 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,146 +1,80 @@
-import random, playsound
-
-def winning_music():
-    """
-    Plays the winning music.
-    """
-    win_music  = ["anime wow.mp3", "bruhh.mp3"]
-    try:
-        playsound.playsound(random.choice(win_music))
-    except Exception as e:
-        print(e)
-
-def losing_music():
-    """
-    Plays the losing music.
-    """
-    lose_music  = ["Nope.mp3", "Fart.mp3"]
-    try:
-        playsound.playsound(random.choice(lose_music))
-    except Exception as e:
-        print(e)
-
-def tie_music():
-    """
-    Plays the tie music.
-    """
-    try:
-        playsound.playsound("Awkward Cricket.mp3")
-    except Exception as e:
-        print(e)
+# Import random module
+import random
+print('Snake - Water - Gun')
 
 
-if __name__ == "__main__":
-    print("\nWelcome to the Snake Water Gun game!\n\n")
-
-    attempts = 1
-    your_point = 0
-    computer_point = 0
+# Input no. of rounds
+n = int(input('Enter number of rounds: '))
 
 
-    while (attempts<=10):
+# List containing Snake(s), Water(w), Gun(g)
+options = ['s', 'w', 'g']
 
-        lst=["s","w","g"]
-        ran=random.choice(lst)
-        
-        inp = input("Enter your choice (Snake(s), Water(w), Gun(g)): ")
-        inp = inp.lower()
+# Round numbers
+rounds = 1
 
-        if inp == ran:
-            print("Tie")
-            print(f"\nYou chose snake and Computer also chose snake! ")
-            print("No body gets point\n")
-            tie_music()
+# Count of computer wins
+comp_win = 0
+
+# Count of player wins
+user_win = 0
 
 
-        elif inp=="s" and ran=="w":
-            your_point=your_point+1
-            print(f"\nYou choosed snake and Computer chose water! ")
-            print("The snake drank water\n")
-            print("You won this round")
-            print("You got 1 point\n")            
-            winning_music()
+# There will be n rounds of game
+while rounds <= n:
+
+	# Display round
+	print(f"Round :{rounds}\nSnake - 's'\nWater - 'w'\nGun - 'g'")
+
+	# Exception handling
+	try:
+		player = input("Choose your option: ")
+	except EOFError as e:
+		print(e)
+
+	# Control of bad inputs
+	if player != 's' and player != 'w' and player != 'g':
+		print("Invalid input, try again\n")
+		continue
+
+	# random.choice() will randomly choose
+	# item from list- options
+	computer = random.choice(options)
+
+	# Conditions based on the game rule
+	if computer == 's':
+		if player == 'w':
+			comp_win += 1
+		elif player == 'g':
+			user_win += 1
+
+	elif computer == 'w':
+		if player == 'g':
+			comp_win += 1
+		elif player == 's':
+			user_win += 1
+
+	elif computer == 'g':
+		if player == 's':
+			comp_win += 1
+		elif player == 'w':
+			user_win += 1
+
+	# Announce winner of every round
+	if user_win > comp_win:
+		print(f"You Won round {rounds}\n")
+	elif comp_win > user_win:
+		print(f"Computer Won round {rounds}\n")
+	else:
+		print("Draw!!\n")
+
+	rounds += 1
 
 
-        elif inp=="w" and ran=="s":
-            computer_point = computer_point + 1
-            print(f"\nYou choosed water and Computer chose snake! ")
-            print("The snake drank water\n")
-            print("You lost this round")
-            print("Computer gets 1 point\n")
-            losing_music()
-
-
-        elif inp=="w" and ran=="g":
-            print(f"\nYou chose water and Computer chose gun! ")
-            your_point = your_point + 1
-            print("The gun sank in water\n")
-            print("You won this round")
-            print("You got 1 point\n")
-            winning_music()
-
-
-        elif inp == "g" and ran == "w":
-            print(f"\nYou choosed gun and Computer chose water! ")
-            computer_point = computer_point + 1
-            print("The gun sank in water\n")
-            print("You Lost this round")
-            print("computer gets 1 point\n")
-            losing_music()
-
-
-        elif inp == "g" and ran == "s":
-            print(f"\nYou choosed gun and Computer chose snake! ")
-            your_point = your_point + 1
-            print("The snake was shot and it died\n")
-            print("You won this round")
-            print("You get 1 point\n")
-            winning_music()
-
-        elif inp == "s" and ran == "g":
-            print(f"\nYou choosed snake and Computer chose gun! ")
-            computer_point=computer_point+1
-            print("The snake was shot and he died\n")
-            print("You lost this round!")
-            print("Computer gets 1 point\n")
-
-        else:
-            print("Invalid input!\n")
-            continue
-
-
-        print("\nNo. of guesses left: {}".format(10 - attempts))
-        attempts = attempts + 1
-
-        if attempts>10:
-            print("\nGame over!\n")
-
-
-    print(f"Your score: {your_point} \nComputer's score: {computer_point}")
-
-    if computer_point > your_point:
-        print("\nComputer won and you lost!\n")
-
-    elif computer_point < your_point:
-        print("\nYou won and computer lost!\n")
-
-    else:
-
-        print("It was a tie!")
-        print("Invalid")
-
-    print(11 - attempts, "no. of guesses left")
-    attempts = attempts + 1
-
-    if attempts>10:
-        print("\nGame over! ")
-
-if computer_point > your_point:
-    print("\nComputer wins and you lose!")
-
-if computer_point < your_point:
-    print("\nYou win and the computer loses!")
-
-print(f"\nYour points are {your_point} and Computer's points are {computer_point}!\n")
-
-
+# Final winner based on the number of wons
+if user_win > comp_win:
+	print("Congratulations!! You Won")
+elif comp_win > user_win:
+	print("You lose!!")
+else:
+	print("Match Draw!!")
